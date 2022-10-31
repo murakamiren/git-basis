557cee4730c4690206a455ceebbbcf5fe3a5a0a7
renmurakami <autoeyy@gmail.com>
2022-10-31 23:15:01 +0900

    jsdoc

--- src/util/add.ts
+++ src/util/add.ts
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param x first number to add
+ * @param y second number to add
+ * @returns result of x + y
+ */
 const add = (x: number, y: number): number => {
 	const result = x + y;
 	return result;
