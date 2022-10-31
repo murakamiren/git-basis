e887d0c0457c6aeac8642fc9d1e4c6c4dd6091b3
renmurakami <autoeyy@gmail.com>
2022-10-31 22:00:07 +0900

    add func

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
+import add from "util/add";
+
 console.log("hello world");
 
 const text = "make dev branch";
 
 const mergeText = "merge from dev branch";
+
+const simpleMath = add(2, 3);
+
+console.log(simpleMath);
